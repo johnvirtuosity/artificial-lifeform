9a7e724075094b7709602811f4147331b4bab1a9
johnvirtuosity <john.kan@virtuosity.co.nz>
2015-05-11 14:57:20 +1200

    Complete adding of 4 direction buttons on page to check posting of messages to messages queue

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,17 +10,11 @@ class MessagesController < ApplicationController
 
   def create
   	@message = params[:message]
-  	#t.string   "direction"
-    #t.text     "message"
-    #t.string   "source_ip"
-    #t.string   "destination_ip"
-    #t.string   "source_mac"
-    #t.string   "destination_mac"
-    #t.text     "remarks"
-
-  	@message = Message.new(direction: 'in',message: params[:message], source_ip: params[:source_ip])
+    @message = Message.new(direction: 'in',message: params[:message], source_ip: request.remote_ip)
   	@message.save
+
   	respond_to do |format|
+      format.html {redirect_to '/messages'}
   	  format.json {render json: params}
   	end
   end
